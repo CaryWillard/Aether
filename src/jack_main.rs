d545cc78b0f1000be0a81c948f5fd6632c35e57d
Cary Willard <carywillard@yahoo.com>
2017-12-26 21:20:42 -0500

    jack_main: Use aether instead of the demo sine

--- src/jack_main.rs
+++ src/jack_main.rs
@@ -8,6 +8,9 @@ use std::io;
 use std::str::FromStr;
 use std::sync::mpsc::channel;
 
+use super::oscillators::{Oscillator, Osc};
+use super::waveforms::Sine;
+
 
 fn read_freq() -> Option<f64> {
     let mut user_input = String::new();
@@ -22,7 +25,7 @@ pub fn run_client() {
     let (client, _status) = Client::new("rust_jack_sine", client_options::NO_START_SERVER).unwrap();
 
     // 2. register port
-    let mut out_port = client.register_port("sine_out", AudioOutSpec::default())
+    let mut out_port = client.register_port("aether_out", AudioOutSpec::default())
         .unwrap();
 
     // 3. define process callback handler
@@ -31,6 +34,10 @@ pub fn run_client() {
     let frame_t = 1.0 / sample_rate as f64;
     let mut time = 0.0;
     let (tx, rx) = channel();
+
+    // Define Oscillators, etc.
+    let mut osc = Osc::new(frequency, sample_rate, Box::new(Sine));
+
     let process = ClosureProcessHandler::new(move |_: &Client, ps: &ProcessScope| -> JackControl {
         // Get output buffer
         let mut out_p = AudioOutPort::new(&mut out_port, ps);
@@ -44,8 +51,9 @@ pub fn run_client() {
 
         // Write output
         for v in out.iter_mut() {
-            let x = frequency * time * 2.0 * f64::consts::PI;
-            let y = x.sin();
+            // let x = frequency * time * 2.0 * f64::consts::PI;
+            // let y = x.sin();
+            let y = osc.get_amplitude();
             *v = y as f32;
             time += frame_t;
         }
